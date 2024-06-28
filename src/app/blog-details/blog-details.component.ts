55b2d3b1314ff297445d1538d8024d373a059660
phamthi <thi.pham@ict-campus.net>
2024-06-28 14:21:40 +0200

    get-post and comment plus css is ok now

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -9,34 +9,63 @@ import {Comment} from "../interfaces/comment-interface";
 import {PostService} from "../services/post.service";
 import {error} from "protractor";
 
-
-
 @Component({
   selector: 'app-blog-details',
   standalone: true,
   imports: [CommonModule],
   template: `
     <article>
+      <div class="antwort">
+        <h2>Frage</h2>
+      </div>
       <section>
+        <div class="user-profil">
+          <img src="../../assets/images/Profil.png">
+          <p>User: {{blog?.account?.name}}</p>
+        </div>
         <h1>Here comes the title: {{ blog?.title }}</h1>
         <p>Here comes the Question: {{ blog?.question }}</p>
-        <p>This is the Questioner: {{ blog?.account?.name }}</p>
+        <p>This are the tags:  still empty now</p>
       </section>
-
-       <section *ngIf="postList?.length">
-        <ul *ngFor="let post of postList">
-            <li>{{post.answer}}</li>
-        </ul>
       
+      <div class="wrap-button">
+        <button type="button" class="post-button">+ Answer</button>
+      </div>
+
+      <div class="antwort">
+        <h2>Antworten</h2>
+      </div>
+      
+      <div class="wrap-answer" *ngFor="let post of postList">
+        <div class="post-listing">
+          <div class="user-profil-answer">
+            <img src="../../assets/images/Profil.png">
+            <p>User: {{post.account?.name}}</p>
+          </div>
+            <p class="post-answer">{{ post.answer }}</p>
+          <div class="post-blc">
+            <button type="button" class="comment-button">+ Answer</button>
+            <img class="post-likes" src="../../assets/images/Like.png">
+            <p> 10</p>
+          </div>
+        </div>
         
-        <section>
+        <div class="comments">
+            <div class="comment-listing" *ngFor="let comment of post.comments">
+              <div class="user-profil-answer">
+                <img src="../../assets/images/Profil.png">
+                <p>User: {{comment?.account?.name}} </p>
+              </div>
+              
+              <p class="comment-comment">{{ comment.comment }}</p>
+            
+              
+            </div>
+        </div>
         
-          <p>Here is a comment. Currently empty</p>
-        </section>
-      </section>
+      </div>
       
     </article>
-
   `,
   styleUrls: ['./blog-details.component.css']
 })
@@ -49,31 +78,77 @@ export class BlogDetailsComponent implements OnInit{
 
   postList: Post[] | undefined;
 
-  commentList: Comment[] | undefined;
+  testPost: Post = {
+    "id": 0,
+    "answer": '',
+    "comments": []
+  }
 
   constructor() {
-    const blogId = Number(this.route.snapshot.params['id']);
-    this.blogService.getBlogById(blogId).then(blog =>{this.blog = blog});
-
-
   }
 
   ngOnInit(){
+    this.loadBlogs();
+    this.loadPostsForBlog();
+    this.loadTestPost();
+    this.loadCommentsTest()
 
   }
 
-  loadPosts(){
+  loadBlogs(){
+    const blogId=Number(this.route.snapshot.params['id']);
+    this.blogService.getBlogById(blogId).then(blog =>{
+      this.blog = blog
+    })
+  }
+
+  loadPostsForBlog(){
     this.blogService.getPostsByBlogId(this.route.snapshot.params['id']).then(
-        posts =>{this.postList = posts},
+        posts =>{this.postList = posts; this.loadCommentsForPost()},
         (error) =>{console.log(error)}
     );
   }
 
-  loadCommentsForPost(){
-    this.postList.forEach(post =>{
-      this.commentList.
+  loadCommentsForPost(): void {
+    this.postList?.forEach(post =>{
+      this.postService.getCommentsByPostId(post.id).then( comments =>{
+        post.comments = comments;
+      }, error =>{ console.log(error)})
     })
+  }
 
+  loadCommentsForPosts(){
+    if (!this.postList) {
+      console.error('postList is undefined');
+      return;
+    }
+    const commentPromises = this.postList.map(post =>
+        this.postService.getCommentsByPostId(post.id).then(
+            comments => {
+              post.comments = comments;
+            },
+            error => {
+              console.error(`Error fetching comments for post ${post.id}:`, error);
+              post.comments = [];
+            }
+        )
+    );
+
+    Promise.all(commentPromises).then(() => {
+      console.log('All comments loaded');
+    }).catch(error => {
+      console.error('Error loading all comments:', error);
+    });
+  }
+
+
+  loadTestPost(){
+    const postId = 1;
+    this.postService.getPostById(postId).then(post => this.testPost = post);
+  }
+  loadCommentsTest(){
+    const postId = 1;
+    this.postService.getCommentsByPostId(postId).then(comment => this.testPost.comments = comment);
   }
 
 
