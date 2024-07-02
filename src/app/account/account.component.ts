d4b81e8a63001c145240f0a77afff3a5e678fc2d
Lucas Rohrbach <lucas.rohrbach@ict-campus.net>
2024-07-02 11:25:22 +0200

    Profile Picture choosing/buying done

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -13,11 +13,11 @@ import {PostService} from "../services/post.service";
             </p>
         <form>
             <label>Select Your Profile Picture</label>
-            <img class="profilePicture" src="https://i.ytimg.com/vi/tzD9OxAHtzU/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLAROSJukM30CxCMoacqsDFlBWSpnA">
+            <img class="profilePicture" (click)="skibidiToiletProfile()" src="https://i.ytimg.com/vi/tzD9OxAHtzU/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLAROSJukM30CxCMoacqsDFlBWSpnA">
             <label>free</label>
-            <img class="profilePicture" src="https://cdn.unitycms.io/images/1H-QVquEqm0AiozooN6LlE.jpg?op=ocroped&val=1200,1200,1000,1000,0,0&sum=xB-n5ww5X7c">
+            <img class="profilePicture" (click)="WomanWhoKnowsProfile()" src="https://cdn.unitycms.io/images/1H-QVquEqm0AiozooN6LlE.jpg?op=ocroped&val=1200,1200,1000,1000,0,0&sum=xB-n5ww5X7c">
             <label>5 Likes</label>
-            <img class="profilePicture" src="https://www.ajc.org/sites/default/files/inline-images/Term%208%20-%20Pepe%20the%20FrogInline-300xflex.jpg">
+            <img class="profilePicture" (click)="PePeProfile()" src="https://www.ajc.org/sites/default/files/inline-images/Term%208%20-%20Pepe%20the%20FrogInline-300xflex.jpg">
             <label>10 Likes</label>
         </form>
     `,
@@ -34,6 +34,28 @@ export class AccountComponent implements OnInit {
             .subscribe((result) => {
                 this.likes = result;
             })
+    }
+    protected skibidiToiletProfile(){
+
+        localStorage.removeItem("profile")
+        localStorage.setItem("profile","SkibidiToilet")
+    }
+    protected WomanWhoKnowsProfile(){
+        if(this.likes >= 5) {
+            localStorage.removeItem("profile")
+            localStorage.setItem("profile", "WomanWhoKnows")
+        }else{
+            alert("You need more Likes")
+            }
+        }
+
+    protected PePeProfile(){
+        if(this.likes >= 10) {
+            localStorage.removeItem("profile")
+            localStorage.setItem("profile", "Pepe")
+        }else{
+            alert("You need more Likes")
+        }
     }
 
 
